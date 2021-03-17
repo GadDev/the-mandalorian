2aec9989ddcae3857a94205859516bc303826d52
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-17 22:37:53 +0000

    creating action

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,20 @@ import './App.css';
 import { Store } from './Store';
 
 function App(): JSX.Element {
-	const store = React.useContext(Store);
+	const {state, dispatch} = React.useContext(Store);
 
+	const URL =
+		'https://api.tvmaze.com/singlesearch/shows?q=the-mandalorian&embed=episodes';
+	const fetchDataAction = async () => {
+		const data = await fetch(URL) 
+		const dataJSON = await data.json()
+		return dispatch({
+			type: 'FETCH_DATA',
+			payload: dataJSON
+		})
+	}
 	return (
 		<React.Fragment>
-			{console.log(store)}
 			<h1>Marvel collection</h1>
 			<p>Pick your favorite character</p>
 		</React.Fragment>
