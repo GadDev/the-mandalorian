b86b41a51cdde5b48b8da8fac38a23207384cf18
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-17 23:07:14 +0000

    a bit of styling

--- src/App.tsx
+++ src/App.tsx
@@ -20,11 +20,45 @@ function App(): JSX.Element {
 		});
 	};
 	return (
-		<React.Fragment>
-			{console.log(state)}
-			<h1>Marvel collection</h1>
-			<p>Pick your favorite character</p>
-		</React.Fragment>
+		<main
+			style={{
+				padding: '2rem',
+				textAlign: 'center',
+			}}
+		>
+			<h1>the mandalorian</h1>
+			<p>Pick your favorite episodes</p>
+			<section
+				style={{
+					display: 'grid',
+					gridTemplateColumns: 'repeat(4, 1fr [col-start])',
+					gridAutoRows: 'grid-template-rows: 25% 100px auto',
+					padding: '2rem',
+				}}
+			>
+				{state.episodes.map((episode: any) => {
+					return (
+						<article key={episode.id}>
+							<div style={{ textAlign: 'center' }}>
+								<img
+									src={episode.image.medium}
+									alt={`The Mandalorian ${episode.name}`}
+								/>
+							</div>
+
+							<h3
+								style={{
+									textAlign: 'center',
+									marginTop: '5px',
+								}}
+							>
+								{episode.name}
+							</h3>
+						</article>
+					);
+				})}
+			</section>
+		</main>
 	);
 }
 
