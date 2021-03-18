d9cff731748e152d57d47a69d6ae5ba8e3bd08d5
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-18 22:18:25 +0000

    Install Mock service worker & Mock definition

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { StoreProvider } from './Store';
 
 test('renders "The mandalorian" title', () => {
-	render(<App />);
+	render(<App />, { wrapper: StoreProvider });
 	const title = screen.getByText(/The mandalorian/i);
+	screen.debug();
 	expect(title).toBeInTheDocument();
 });
