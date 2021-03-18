4d0fe099cd8f925655f07c8e9cd50f9a8a48b4db
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-18 18:35:51 +0000

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders "Hello world" title', () => {
+test('renders "The mandalorian" title', () => {
 	render(<App />);
-	const title = screen.getByText(/Marvel collection/i);
+	const title = screen.getByText(/The mandalorian/i);
 	expect(title).toBeInTheDocument();
 });
