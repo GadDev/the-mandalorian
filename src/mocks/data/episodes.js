d9c998ff8862af38bf3b50e150b4d08194c70934
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-18 22:34:11 +0000

    set handlers and Response resolver

---
+++ src/mocks/data/episodes.js
@@ -0,0 +1,44 @@
+export const episodes = [
+	{
+		airdate: '2019-12-06',
+		airstamp: '2019-12-06T12:00:00+00:00',
+		airtime: '',
+		id: 1740394,
+		image: {
+			medium:
+				'https://static.tvmaze.com/uploads/images/medium_landscape/230/575424.jpg',
+			original:
+				'https://static.tvmaze.com/uploads/images/original_untouched/230/575424.jpg',
+		},
+		name: 'Chapter 5: The Gunslinger',
+		number: 5,
+		runtime: 35,
+		season: 1,
+		summary:
+			'<p>The Mandalorian helps a rookie bounty hunter who is in over his head.</p>',
+		type: 'regular',
+		url:
+			'https://www.tvmaze.com/episodes/1740394/the-mandalorian-1x05-chapter-5-the-gunslinger',
+	},
+	{
+		airdate: '2019-12-27',
+		airstamp: '2019-12-27T12:00:00+00:00',
+		airtime: '',
+		id: 1740397,
+		image: {
+			medium:
+				'https://static.tvmaze.com/uploads/images/medium_landscape/233/583218.jpg',
+			original:
+				'https://static.tvmaze.com/uploads/images/original_untouched/233/583218.jpg',
+		},
+		name: 'Chapter 8: Redemption',
+		number: 8,
+		runtime: 49,
+		season: 1,
+		summary:
+			'<p>The Mandalorian comes face-to-face with an unexpected enemy.</p>',
+		type: 'regular',
+		url:
+			'https://www.tvmaze.com/episodes/1740397/the-mandalorian-1x08-chapter-8-redemption',
+	},
+];
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,2 +1,13 @@
 // src/mocks/handlers.js
 import { rest } from 'msw';
+import { episodes } from './data/episodes'
+
+const URL =
+	'https://api.tvmaze.com/singlesearch/shows?q=the-mandalorian&embed=episodes'; 
+
+export const handlers = [rest.get(URL,(req,res,ctx) => {
+	return res(
+		ctx.status(200),
+		ctx.json(episodes)
+	)
+})];
