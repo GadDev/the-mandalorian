e6b7330333a6d773347c1df301fb3dd6e5b02f3c
Alex Gadaix <alex.gadaix@gmail.com>
2021-03-19 23:13:54 +0000

    import episodeslist

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Store } from '../Store';
 import { IAction, IEpisode } from '../interfaces';
 
+const EpisodesList = React.lazy<any>(() => import('../components/EpisodesList'));
+
 export default function HomePage(): JSX.Element {
 	const { state, dispatch } = React.useContext(Store);
 	React.useEffect(() => {
@@ -38,6 +40,14 @@ export default function HomePage(): JSX.Element {
 		return dispatch(dispatchObj);
 	};
 	return (
-		<div></div>
-	)
+		<React.Suspense fallback={<div>Loading...</div>}>
+			<section className='episode-wrapper'>
+				<EpisodesList
+					episodes={state.episodes}
+					favourites={state.favourites}
+					toggleFavAction={toggleFavAction}
+				/>
+			</section>
+		</React.Suspense>
+	);
 }
